187745f3eba750d64e9724a6d82d13a7c35ceffc
peterpisljar <peterpisljar@gmail.com>
2014-06-26 13:53:48 +0200

    Create helper.js

---
+++ helper.js
@@ -0,0 +1,51 @@
+function buildsb(keyword) {
+	console.log("starting...");
+	var st = new Date().getTime();
+	var cnt = find_string('body', keyword);
+	var et = new Date().getTime();
+	console.log(et-st);
+	st=et;
+	var sb = $.fn.scrollIndicatorBar({append: false});
+	var et = new Date().getTime();
+	console.log(et-st);
+	st=et;
+	for (i=0; i < cnt; i++ ) {
+		sb.addMarker(".match_indicator_"+i);
+	}
+	var et = new Date().getTime();
+	console.log(et-st);
+	st=et;
+	$(document.body).append(sb);
+	var et = new Date().getTime();
+	console.log(et-st);
+	st=et;
+}
+
+function find_string(container,what,spanClass) {		
+	var el = $(container)[0],
+		content = el.innerHTML,
+		re = new RegExp(what, "ig"),
+		startTag = "",
+		endTag = "</span>";
+	
+	var output = "";
+	var counter = 0;
+	while (true) {
+		match = re.exec(content);
+		if (match == null) break;
+		
+		startTag = "<span style='color:red !important' class='match_indicator_"+(counter++)+"'>";
+		
+		output += [content.slice(0, match.index), startTag].join('');
+		output += [content.substr(match.index, re.lastIndex-match.index), endTag].join('');
+		content = content.slice(re.lastIndex);
+		re.lastIndex = 0;
+	}
+	
+	el.innerHTML = output;		
+	return counter;
+}
+	
+$(document).ready(function() {
+	buildsb("naive");
+});
